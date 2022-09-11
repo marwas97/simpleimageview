fd141fa0b904ac2bd2e21adb932ee8efe0d6402b
marek wasiewski <108329227+marwas97@users.noreply.github.com>
2022-09-11 13:35:42 +0200

    removed music

--- src/main/java/com/example/simpleimageview/HelloApplication.java
+++ src/main/java/com/example/simpleimageview/HelloApplication.java
@@ -7,14 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static com.example.simpleimageview.HelloController.playMusic;
-
 public class HelloApplication extends Application {
 
 
     @Override
     public void start(Stage stage) throws IOException {
-        playMusic();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("xd");
